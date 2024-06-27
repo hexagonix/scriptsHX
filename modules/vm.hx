ed4dec5c3f4726990b87ec1dddbfe2532b749e3e
Felipe Lunkes <felipenldev@gmail.com>
2024-06-27 01:48:28 -0300

    v0.1
    Code formatting

--- modules/vm.hx
+++ modules/vm.hx
@@ -1,7 +1,89 @@
 #!/bin/bash
+#
+#*************************************************************************************************
+#
+# 88                                                                                88
+# 88                                                                                ""
+# 88
+# 88,dPPPba,   ,adPPPba, 8b,     ,d8 ,adPPPPba,  ,adPPPb,d8  ,adPPPba,  8b,dPPPba,  88 8b,     ,d8
+# 88P'    "88 a8P     88  `P8, ,8P'  ""     `P8 a8"    `P88 a8"     "8a 88P'   `"88 88  `P8, ,8P'
+# 88       88 8PP"""""""    )888(    ,adPPPPP88 8b       88 8b       d8 88       88 88    )888(
+# 88       88 "8b,   ,aa  ,d8" "8b,  88,    ,88 "8a,   ,d88 "8a,   ,a8" 88       88 88  ,d8" "8b,
+# 88       88  `"Pbbd8"' 8P'     `P8 `"8bbdP"P8  `"PbbdP"P8  `"PbbdP"'  88       88 88 8P'     `P8
+#                                               aa,    ,88
+#                                                "P8bbdP"
+#
+#                    Sistema Operacional Hexagonix - Hexagonix Operating System
+#
+#                         Copyright (c) 2015-2024 Felipe Miguel Nery Lunkes
+#                        Todos os direitos reservados - All rights reserved.
+#
+#*************************************************************************************************
+#
+# Português:
+#
+# O Hexagonix e seus componentes são licenciados sob licença BSD-3-Clause. Leia abaixo
+# a licença que governa este arquivo e verifique a licença de cada repositório para
+# obter mais informações sobre seus direitos e obrigações ao utilizar e reutilizar
+# o código deste ou de outros arquivos.
+#
+# English:
+#
+# Hexagonix and its components are licensed under a BSD-3-Clause license. Read below
+# the license that governs this file and check each repository's license for
+# obtain more information about your rights and obligations when using and reusing
+# the code of this or other files.
+#
+#*************************************************************************************************
+#
+# BSD 3-Clause License
+#
+# Copyright (c) 2015-2024, Felipe Miguel Nery Lunkes
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# $HexagonixOS$
 
-vmHexagonixPentium3()
-{
+function main() {
+
+case $1 in
+
+hx) vmHexagonixWithoutSnd; exit;;
+hx.snd) vmHexagonixWithKVM; exit;;
+hx.bsd) vmHexagonixOnBSDHost; exit;;
+hx.serial) vmHexagonixWithoutSerialRedirection; exit;;
+hx.p3) vmHexagonixPentium3; exit;;
+*) vmHexagonixWithoutSnd; exit;; # Assume hx -v hx
+
+esac
+
+}
+
+function vmHexagonixPentium3() {
 
 QEMU_ARGS="--enable-kvm -serial file:Serial.txt -cpu $PROCESSOR -hda $DISK_IMAGE_PATH -m $MEMORY -audiodev $AUDIO_DEVICE -k pt-br"
 NOTA="Using KVM and legacy processor (Pentium III)"
@@ -10,8 +92,7 @@ startVirtualMachine
 
 }
 
-vmHexagonixWithoutSnd()
-{
+function vmHexagonixWithoutSnd() {
 
 QEMU_ARGS="--enable-kvm -serial file:Serial.txt -cpu host -hda $DISK_IMAGE_PATH -m $MEMORY -k pt-br"
 DRV_SOUND="none"
@@ -21,8 +102,7 @@ startVirtualMachine
 
 }
 
-vmHexagonixOnBSDHost()
-{
+function vmHexagonixOnBSDHost() {
 
 QEMU_ARGS="-cpu $PROCESSOR -hda $DISK_IMAGE_PATH -m $MEMORY -k pt-br"
 NOTA="BSD mode"
@@ -31,8 +111,7 @@ startVirtualMachine
 
 }
 
-vmHexagonixWithKVM()
-{
+function vmHexagonixWithKVM() {
 
 QEMU_ARGS="--enable-kvm -serial file:Serial.txt -cpu host -hda $DISK_IMAGE_PATH -m $MEMORY -audiodev $AUDIO_DEVICE -k pt-br"
 NOTA="Using KVM and serial output to file"
@@ -41,8 +120,7 @@ startVirtualMachine
 
 }
 
-vmHexagonixWithoutSerialRedirection()
-{
+function vmHexagonixWithoutSerialRedirection() {
 
 QEMU_ARGS="-serial stdio -hda $DISK_IMAGE_PATH -cpu $PROCESSOR -m $MEMORY -k pt-br"
 NOTA="Using serial output to console"
@@ -51,8 +129,7 @@ startVirtualMachine
 
 }
 
-startVirtualMachine()
-{
+function startVirtualMachine() {
 
 if [ -e $DISK_IMAGE_PATH ] ; then
 
@@ -81,8 +158,7 @@ fi
 
 }
 
-virtualMachineGeneralError()
-{
+function virtualMachineGeneralError() {
 
 clear
 
@@ -99,7 +175,11 @@ echo
 
 }
 
+# Imports
+
 . $MOD_DIR/common.hx
+
+# Constants
 
 MOD_VER="0.1"
 
@@ -112,14 +192,4 @@ PROCESSOR="pentium3"
 MEMORY=32
 AUDIO_DEVICE="pa,id=audio0 -machine pcspk-audiodev=audio0"
 
-case $1 in
-
-hx) vmHexagonixWithoutSnd; exit;;
-hx.snd) vmHexagonixWithKVM; exit;;
-hx.bsd) vmHexagonixOnBSDHost; exit;;
-hx.serial) vmHexagonixWithoutSerialRedirection; exit;;
-hx.p3) vmHexagonixPentium3; exit;;
-*) vmHexagonixWithoutSnd; exit;; # Assume hx -v hx
-
-esac
-
+main $1
